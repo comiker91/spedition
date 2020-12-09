2c8e0333f97850739a150e4f537e14bfe4879280
comiker91 <mundt110@freenet.de>
2020-12-09 13:55:07 +0100

    09-01-2020 13:55

--- spedition/verwaltung/views.py
+++ spedition/verwaltung/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import *
 
 # Create your views here.
 
 def index(request):
     template = 'spedition/index.html'
     return render(request,template,{})
+
+def bestellung(request):
+    if request.method =="GET":
+        template = 'spedition/bestellung.html'
+        kundends = Kunde.objects.all()
+        return render(request,template,{'kunden':kundends})
+    else:
+        button = request.POST['button']
+        if button == "cancel":
+            return redirect("/")
+        else:
+            bestellungen = request.POST['bestellung'] 
+            kunde = Kunde.objects.get(name=(request.POST['kunde']))
+            print(kunde)
+            ds = Bestellung(bestellung=bestellungen,kunde=kunde)
+            ds.save()
+            return redirect("/")
